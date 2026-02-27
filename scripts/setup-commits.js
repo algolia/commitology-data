32ecfeab00893442957ca75ed31c8689b4d16233
Tim Carry <tim@pixelastic.com>
2026-02-27 11:52:16 +0100

    data(commits): Include full body, timestamp and author

--- scripts/setup-commits.js
+++ scripts/setup-commits.js
@@ -1,7 +1,7 @@
-import { pMap } from 'golgoth';
+import { _, pMap } from 'golgoth';
 import { absolute, spinner, writeJson } from 'firost';
-import { dataInputCommitsPath } from '../lib/config.js';
-import { getCommitList } from '../lib/git.js';
+import { commitFieldOrder, dataInputCommitsPath } from '../lib/config.js';
+import { getCommitData, getCommitList } from '../lib/git.js';
 
 // We define the path to instantsearch
 // We find all commits in instantsearch
@@ -11,21 +11,25 @@ import { getCommitList } from '../lib/git.js';
 // Because there will be a lot of file, we might need to split into YYYY/MM
 // subfolders
 
-const commitList = await getCommitList();
+const commitList = _.slice(await getCommitList(), 0, 12000);
 const progress = spinner(commitList.length);
 
 await pMap(
   commitList,
   async (rawCommit) => {
     const { datePath, hash, subject } = rawCommit;
-    const commitPath = absolute(dataInputCommitsPath, datePath, `${hash}.json`);
-    const commitContent = {
-      hash,
-      subject,
-    };
-    await writeJson(commitContent, commitPath);
     progress.tick(subject);
+
+    // Commit path
+    const commitPath = absolute(dataInputCommitsPath, datePath, `${hash}.json`);
+
+    // Commit content
+    const commitData = await getCommitData(hash);
+
+    await writeJson(commitData, commitPath, {
+      sort: commitFieldOrder,
+    });
   },
-  { concurrency: 10 },
+  { concurrency: 50 },
 );
 progress.success('All commits imported');
