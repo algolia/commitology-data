f38d7eba5ad352b0ebf2b617a075e31e114c9263
Tim Carry <tim@pixelastic.com>
2026-03-11 09:34:21 +0100

    Generate all commits

--- scripts/generate/commits.js
+++ scripts/generate/commits.js
@@ -1,5 +1,5 @@
 import { _, pMap } from 'golgoth';
-import { absolute, glob, readJson, writeJson } from 'firost';
+import { absolute, glob, readJson, spinner, writeJson } from 'firost';
 import {
   dataInputCommitsPath,
   dataOutputCommitsPath,
@@ -8,29 +8,36 @@ import {
 import { datePath } from '../../lib/helper.js';
 
 const rawCommitPaths = await glob('./**/*.json', { cwd: dataInputCommitsPath });
+const progress = spinner(rawCommitPaths.length);
 
-await pMap(_.slice(rawCommitPaths, -1), async (rawCommitPath) => {
-  const rawCommitData = await readJson(rawCommitPath);
-  const { author, subject, hash, body, date } = rawCommitData;
+await pMap(
+  rawCommitPaths,
+  async (rawCommitPath) => {
+    const rawCommitData = await readJson(rawCommitPath);
+    const { author, subject, hash, body, date } = rawCommitData;
+    progress.tick(subject);
 
-  const shortHash = hash.substring(0, 7);
-  const bodyLines = _.split(body, '\n');
+    const shortHash = hash.substring(0, 7);
+    const bodyLines = _.split(body, '\n');
 
-  const commitData = {
-    hash: shortHash,
-    author,
-    subject,
-    bodyLines,
-    date,
-  };
+    const commitData = {
+      hash: shortHash,
+      author,
+      subject,
+      bodyLines,
+      date,
+    };
 
-  const commitPath = absolute(
-    dataOutputCommitsPath,
-    datePath(date),
-    `${shortHash}.json`,
-  );
+    const commitPath = absolute(
+      dataOutputCommitsPath,
+      datePath(date),
+      `${shortHash}.json`,
+    );
 
-  await writeJson(commitData, commitPath, {
-    sort: outputCommitFieldOrder,
-  });
-});
+    await writeJson(commitData, commitPath, {
+      sort: outputCommitFieldOrder,
+    });
+  },
+  { concurrency: 50 },
+);
+progress.success('All commits generated');
