95eb5e04779c2f53098fd2f3a979a9c986ba06d2
Tim Carry <tim@pixelastic.com>
2026-03-17 09:41:15 +0100

    data(comments): output comments

---
+++ scripts/output/comments.js
@@ -0,0 +1,25 @@
+import { spinner, writeJson } from 'firost';
+import {
+  fieldOrder,
+  forEachInputComment,
+  getOutputPath,
+  normalizeComment,
+} from '../../lib/helpers/comment.js';
+
+const progress = spinner();
+
+await forEachInputComment(
+  async (inputComment) => {
+    const { index, max, data } = inputComment;
+    progress.tick(`[${index}/${max}] Comment #${data.id}`);
+
+    const commentData = await normalizeComment(data);
+    const commentPath = getOutputPath(commentData);
+
+    await writeJson(commentData, commentPath, {
+      sort: fieldOrder,
+    });
+  },
+  { concurrency: 50 },
+);
+progress.success('All comments generated');
